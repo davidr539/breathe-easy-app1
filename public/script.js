27b63e210451be5a2b4c3a0ba8905bb0f771f262
davidr539 <164889683+davidr539@users.noreply.github.com>
2024-06-06 08:31:47 -0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    //add a comment here that explains the fetching and processing of data    
+  //this code gets the air quality data from /api/data and makes it into JSON 
+  //The data is then mapped to extract labels and values
     const response = await fetch('/api/data');
     const data = await response.json();
 
     const labels = data.map(item => new Date(item.observationtimeutc).toLocaleString());
     const values = data.map(item => item.value);
 
-    //comment explaining the creation and configuration of the chart.js chart
+    //gets air quailty data and then uses chart.js to make a line chart with the data
     const ctx = document.getElementById('myChart').getContext('2d');
     const myChart = new Chart(ctx, {
         type: 'line',
