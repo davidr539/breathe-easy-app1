2a3e014448d64277674403cb507ba01867ae7826
davidr539 <164889683+davidr539@users.noreply.github.com>
2024-06-06 08:39:12 -0600

    Update data.js

--- routes/data.js
+++ routes/data.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const router = express.Router();
 
-//comment on what this code (lines 7-13) does
+//configures how the application will connect to a PostgreSQL database
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
@@ -12,7 +12,7 @@ const pool = new Pool({
     port: process.env.DB_PORT
 });
 
-// Route to fetch all observation data
+//Route to fetch all observation data
 router.get('/data', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM observations');
